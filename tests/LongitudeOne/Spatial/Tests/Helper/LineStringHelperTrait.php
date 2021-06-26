4523cc05844e0fd26bc69525b48c6e76eb5ace5e
Alexandre Tranchant <alexandre.tranchant@mail.com>
2021-06-26 15:07:46 +0200

    LineString Helpers trait improved

--- tests/LongitudeOne/Spatial/Tests/Helper/LineStringHelperTrait.php
+++ tests/LongitudeOne/Spatial/Tests/Helper/LineStringHelperTrait.php
@@ -39,47 +39,205 @@ use LongitudeOne\Spatial\Tests\Fixtures\LineStringEntity;
 trait LineStringHelperTrait
 {
     /**
+     * Create a broken linestring.
+     * Line is created with three aligned points: (3 3) (4 15) (5 22).
+     */
+    protected function createAngularLineString(): LineString
+    {
+        try {
+            return new LineString([
+                new Point(3, 3),
+                new Point(4, 15),
+                new Point(5, 22),
+            ]);
+        } catch (InvalidValueException $e) {
+            static::fail(sprintf('Unable to create angular linestring: %s', $e->getMessage()));
+        }
+    }
+
+    /**
+     * Create a linestring A.
+     * Line is created with two points: (0 0, 10 10).
+     */
+    protected function createLineStringA(): LineString
+    {
+        try {
+            return new LineString([
+                new Point(0, 0),
+                new Point(10, 10),
+            ]);
+        } catch (InvalidValueException $e) {
+            static::fail(sprintf('Unable to create linestring A (0 0, 10 10): %s', $e->getMessage()));
+        }
+    }
+
+    /**
+     * Create a linestring B.
+     * Line B crosses lines A and C.
+     * Line is created with two points: (0 10, 15 0).
+     */
+    protected function createLineStringB(): LineString
+    {
+        try {
+            return new LineString([
+                new Point(0, 10),
+                new Point(15, 0),
+            ]);
+        } catch (InvalidValueException $e) {
+            static::fail(sprintf('Unable to create linestring B (0 10, 15 0): %s', $e->getMessage()));
+        }
+    }
+
+    /**
+     * Create a linestring C.
+     * Linestring C does not cross linestring A.
+     * Linestring C crosses linestring B.
+     * Line is created with two points: (2 0, 12 10).
+     */
+    protected function createLineStringC(): LineString
+    {
+        try {
+            return new LineString([
+                new Point(2, 0),
+                new Point(12, 10),
+            ]);
+        } catch (InvalidValueException $e) {
+            static::fail(sprintf('Unable to create linestring C (2 0, 12 10): %s', $e->getMessage()));
+        }
+    }
+
+    /**
+     * Create a linestring X.
+     * Line is created with two points: (8 15, 4 8).
+     */
+    protected function createLineStringX(): LineString
+    {
+        try {
+            return new LineString([
+                new Point(8, 15),
+                new Point(4, 8),
+            ]);
+        } catch (InvalidValueException $e) {
+            static::fail(sprintf('Unable to create linestring X (8 15, 4 8): %s', $e->getMessage()));
+        }
+    }
+
+    /**
+     * Create a linestring Y.
+     * Line is created with two points: (12 14, 3 4).
+     */
+    protected function createLineStringY(): LineString
+    {
+        try {
+            return new LineString([
+                new Point(12, 14),
+                new Point(3, 4),
+            ]);
+        } catch (InvalidValueException $e) {
+            static::fail(sprintf('Unable to create linestring Y (12 14, 3 4): %s', $e->getMessage()));
+        }
+    }
+
+    /**
+     * Create a linestring Z
+     * Line is created with five points: (2 5, 3 6, 12 8, 10 10, 13 11).
+     */
+    protected function createLineStringZ(): LineString
+    {
+        try {
+            return new LineString([
+                new Point(2, 5),
+                new Point(3, 6),
+                new Point(12, 8),
+                new Point(10, 10),
+                new Point(13, 11),
+            ]);
+        } catch (InvalidValueException $e) {
+            static::fail(sprintf('Unable to create linestring Z: %s', $e->getMessage()));
+        }
+    }
+
+    /**
+     * Create a node linestring.
+     * Line is crossing herself like butterfly node: (0 0) (1 0) (0 1) (1 1) (0 0).
+     */
+    protected function createNodeLineString(): LineString
+    {
+        try {
+            return new LineString([
+                new Point(0, 0),
+                new Point(1, 0),
+                new Point(0, 1),
+                new Point(1, 1),
+                new Point(0, 0),
+            ]);
+        } catch (InvalidValueException $e) {
+            static::fail(sprintf('Unable to create butterfly node linestring: %s', $e->getMessage()));
+        }
+    }
+
+    /**
+     * Create a ring linestring.
+     * Line is like a square (0 0, 11) with 4 points: (0 0) (1 0) (1 1) (0 1) (0 0).
+     */
+    protected function createRingLineString(): LineString
+    {
+        try {
+            return new LineString([
+                new Point(0, 0),
+                new Point(1, 0),
+                new Point(1, 1),
+                new Point(0, 1),
+                new Point(0, 0),
+            ]);
+        } catch (InvalidValueException $e) {
+            static::fail(sprintf('Unable to create a ring linestring: %s', $e->getMessage()));
+        }
+    }
+
+    /**
+     * Create a straight linestring.
+     * Line is created with three aligned points: (0 0) (2 2) (5 5).
+     */
+    protected function createStraightLineString(): LineString
+    {
+        try {
+            return new LineString([
+                new Point(0, 0),
+                new Point(2, 2),
+                new Point(5, 5),
+            ]);
+        } catch (InvalidValueException $e) {
+            static::fail(sprintf('Unable to create straight linestring: %s', $e->getMessage()));
+        }
+    }
+
+    /**
      * Create a broken linestring and persist it in database.
      * Line is created with three aligned points: (3 3) (4 15) (5 22).
-     *
-     * @throws InvalidValueException when geometries are not valid
      */
     protected function persistAngularLineString(): LineStringEntity
     {
-        return $this->persistLineString([
-            new Point(3, 3),
-            new Point(4, 15),
-            new Point(5, 22),
-        ]);
+        return $this->persistLineString($this->createAngularLineString());
     }
 
     /**
      * Create a linestring A and persist it in database.
      * Line is created with two points: (0 0, 10 10).
-     *
-     * @throws InvalidValueException when geometries are not valid
      */
     protected function persistLineStringA(): LineStringEntity
     {
-        return $this->persistLineString([
-            new Point(0, 0),
-            new Point(10, 10),
-        ]);
+        return $this->persistLineString($this->createLineStringA());
     }
 
     /**
      * Create a linestring B and persist it in database.
      * Line B crosses lines A and C.
      * Line is created with two points: (0 10, 15 0).
-     *
-     * @throws InvalidValueException when geometries are not valid
      */
     protected function persistLineStringB(): LineStringEntity
     {
-        return $this->persistLineString([
-            new Point(0, 10),
-            new Point(15, 0),
-        ]);
+        return $this->persistLineString($this->createLineStringB());
     }
 
     /**
@@ -87,106 +245,64 @@ trait LineStringHelperTrait
      * Linestring C does not cross linestring A.
      * Linestring C crosses linestring B.
      * Line is created with two points: (2 0, 12 10).
-     *
-     * @throws InvalidValueException when geometries are not valid
      */
     protected function persistLineStringC(): LineStringEntity
     {
-        return $this->persistLineString([
-            new Point(2, 0),
-            new Point(12, 10),
-        ]);
+        return $this->persistLineString($this->createLineStringC());
     }
 
     /**
      * Create a linestring X and persist it in database.
-     *
-     * @throws InvalidValueException when geometries are not valid
+     * Line is created with two points: (8 15, 4 8).
      */
     protected function persistLineStringX(): LineStringEntity
     {
-        return $this->persistLineString([
-            new Point(8, 15),
-            new Point(4, 8),
-        ]);
+        return $this->persistLineString($this->createLineStringX());
     }
 
     /**
      * Create a linestring Y and persist it in database.
-     *
-     * @throws InvalidValueException when geometries are not valid
+     * Line is created with two points: (12 14, 3 4).
      */
     protected function persistLineStringY(): LineStringEntity
     {
-        return $this->persistLineString([
-            new Point(12, 14),
-            new Point(3, 4),
-        ]);
+        return $this->persistLineString($this->createLineStringY());
     }
 
     /**
      * Create a linestring Z and persist it in database.
-     *
-     * @throws InvalidValueException when geometries are not valid
+     * Line is created with five points: (2 5, 3 6, 12 8, 10 10, 13 11).
      */
     protected function persistLineStringZ(): LineStringEntity
     {
-        return $this->persistLineString([
-            new Point(2, 5),
-            new Point(3, 6),
-            new Point(12, 8),
-            new Point(10, 10),
-            new Point(13, 11),
-        ]);
+        return $this->persistLineString($this->createLineStringZ());
     }
 
     /**
      * Create a node linestring and persist it in database.
-     * Line is created with three aligned points: (0 0) (1 0) (0 1) (1 1) (0 0).
-     *
-     * @throws InvalidValueException when geometries are not valid
+     * Line is crossing herself like butterfly node: (0 0) (1 0) (0 1) (1 1) (0 0).
      */
     protected function persistNodeLineString(): LineStringEntity
     {
-        return $this->persistLineString([
-            new Point(0, 0),
-            new Point(1, 0),
-            new Point(0, 1),
-            new Point(1, 1),
-            new Point(0, 0),
-        ]);
+        return $this->persistLineString($this->createNodeLineString());
     }
 
     /**
      * Create a ring linestring and persist it in database.
-     * Line is created with three aligned points: (0 0) (1 0) (1 1) (0 1) (0 0).
-     *
-     * @throws InvalidValueException when geometries are not valid
+     * Line is like a square (0 0, 11) with 4 points: (0 0) (1 0) (1 1) (0 1) (0 0).
      */
     protected function persistRingLineString(): LineStringEntity
     {
-        return $this->persistLineString([
-            new Point(0, 0),
-            new Point(1, 0),
-            new Point(1, 1),
-            new Point(0, 1),
-            new Point(0, 0),
-        ]);
+        return $this->persistLineString($this->createRingLineString());
     }
 
     /**
      * Create a straight linestring and persist it in database.
      * Line is created with three aligned points: (0 0) (2 2) (5 5).
-     *
-     * @throws InvalidValueException when geometries are not valid
      */
     protected function persistStraightLineString(): LineStringEntity
     {
-        return $this->persistLineString([
-            new Point(0, 0),
-            new Point(2, 2),
-            new Point(5, 5),
-        ]);
+        return $this->persistLineString($this->createStraightLineString());
     }
 
     /**
@@ -204,14 +320,12 @@ trait LineStringHelperTrait
     /**
      * Create a LineString entity from an array of points.
      *
-     * @param Point[] $points the array of points
-     *
-     * @throws InvalidValueException when geometries are not valid
+     * @param LineString $linestring the LineString object to persist
      */
-    private function persistLineString(array $points): LineStringEntity
+    private function persistLineString(LineString $linestring): LineStringEntity
     {
         $lineStringEntity = new LineStringEntity();
-        $lineStringEntity->setLineString(new LineString($points));
+        $lineStringEntity->setLineString($linestring);
         $this->getEntityManager()->persist($lineStringEntity);
 
         return $lineStringEntity;
