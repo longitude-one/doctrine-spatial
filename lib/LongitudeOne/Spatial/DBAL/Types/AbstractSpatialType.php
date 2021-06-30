c29767bc94e94f3dc439b6c41cc931ce8bf70d0f
Alexandre Tranchant <alexandre.tranchant@mail.com>
2021-06-30 09:34:13 +0200

    Quality code verified

--- lib/LongitudeOne/Spatial/DBAL/Types/AbstractSpatialType.php
+++ lib/LongitudeOne/Spatial/DBAL/Types/AbstractSpatialType.php
@@ -58,8 +58,7 @@ abstract class AbstractSpatialType extends Type
      * @param GeometryInterface $value    the value to convert
      * @param AbstractPlatform  $platform the database platform
      *
-     * @throws InvalidValueException        when value is not of type Geometry interface
-     * @throws UnsupportedPlatformException when platform is not declared in constant
+     * @throws UnsupportedPlatformException|InvalidValueException when value is not an instance of Geometry Interface
      *
      * @return string|null
      */
