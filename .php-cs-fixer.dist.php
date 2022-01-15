e37245a962d8dc515ca2aeebc212b3edd2414fcc
Alexandre-T <alexandre.tranchant@gmail.com>
2022-01-15 12:44:32 +0100

    Headers updated

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,10 +9,10 @@ $finder = PhpCsFixer\Finder::create()
 $header = <<<EOF
 This file is part of the doctrine spatial extension.
 
-PHP 7.4 | 8.0
+PHP 7.4 | 8.0 | 8.1
 
-(c) Alexandre Tranchant <alexandre.tranchant@gmail.com> 2017 - 2021
-(c) Longitude One 2020 - 2021 
+(c) Alexandre Tranchant <alexandre.tranchant@gmail.com> 2017 - 2022
+(c) Longitude One 2020 - 2022 
 (c) 2015 Derek J. Lambert
 
 For the full copyright and license information, please view the LICENSE
