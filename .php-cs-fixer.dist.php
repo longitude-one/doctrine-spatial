c6be7f995b9190c3773d22a6d9269222aeb3ca84
Alexis Urien <alexis@smiile.com>
2023-04-13 09:53:02 +0200

    Remove php7.4 & 8.0 support

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the doctrine spatial extension.
  *
- * PHP 7.4 | 8.0 | 8.1
+ * PHP 8.1
  *
  * (c) Alexandre Tranchant <alexandre.tranchant@gmail.com> 2017 - 2022
  * (c) Longitude One 2020 - 2022
@@ -22,7 +22,7 @@ $finder = PhpCsFixer\Finder::create()
 $header = <<<'EOF'
     This file is part of the doctrine spatial extension.
 
-    PHP 7.4 | 8.0 | 8.1
+    PHP 8.1
 
     (c) Alexandre Tranchant <alexandre.tranchant@gmail.com> 2017 - 2022
     (c) Longitude One 2020 - 2022 
