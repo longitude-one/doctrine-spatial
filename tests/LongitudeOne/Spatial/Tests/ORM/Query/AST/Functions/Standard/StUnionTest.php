1ee5daf3c00c25178d7f73e15aa406bf7d11ba2b
Alexandre Tranchant <alexandre.Tranchant@gmail.com>
2024-03-11 13:46:39 +0100

    Update copyrights and fix #44 (#45)
    * Fixing #44
    
    * Fixing headers

--- tests/LongitudeOne/Spatial/Tests/ORM/Query/AST/Functions/Standard/StUnionTest.php
+++ tests/LongitudeOne/Spatial/Tests/ORM/Query/AST/Functions/Standard/StUnionTest.php
@@ -4,9 +4,9 @@
  *
  * PHP 8.1
  *
- * (c) Alexandre Tranchant <alexandre.tranchant@gmail.com> 2017 - 2022
- * (c) Longitude One 2020 - 2022
- * (c) 2015 Derek J. Lambert
+ * Copyright Alexandre Tranchant <alexandre.tranchant@gmail.com> 2017-2024
+ * Copyright Longitude One 2020-2024
+ * Copyright 2015 Derek J. Lambert
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
