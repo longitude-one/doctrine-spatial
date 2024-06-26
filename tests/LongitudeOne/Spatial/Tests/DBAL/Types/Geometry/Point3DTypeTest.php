a95958cf3517112465c9f49e2407772cbdc1c445
Alexandre Tranchant <alexandre.tranchant@gmail.com>
2024-06-26 13:05:29 +0200

    Test geographic and geometric 3d points.

---
+++ tests/LongitudeOne/Spatial/Tests/DBAL/Types/Geometry/Point3DTypeTest.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * This file is part of the doctrine spatial extension.
+ *
+ * PHP          8.1 | 8.2 | 8.3
+ * Doctrine ORM 2.19 | 3.1
+ *
+ * Copyright Alexandre Tranchant <alexandre.tranchant@gmail.com> 2017-2024
+ * Copyright Longitude One 2020-2024
+ * Copyright 2015 Derek J. Lambert
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace LongitudeOne\Spatial\Tests\DBAL\Types\Geometry;
+
+use Doctrine\DBAL\ParameterType;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+use Doctrine\DBAL\Types\Exception\TypeNotRegistered;
+use Doctrine\DBAL\Types\Type;
+use LongitudeOne\Spatial\DBAL\Types\Geometry\Point3DType;
+use LongitudeOne\Spatial\DBAL\Types\Geometry\PointType;
+use LongitudeOne\Spatial\PHP\Types\Geometry\Point3D;
+use LongitudeOne\Spatial\Tests\Fixtures\Point3DEntity;
+use LongitudeOne\Spatial\Tests\Helper\PersistantPointHelperTrait;
+use LongitudeOne\Spatial\Tests\PersistOrmTestCase;
+
+/**
+ * Doctrine PointType tests.
+ *
+ * @author  Derek J. Lambert <dlambert@dereklambert.com>
+ * @license https://dlambert.mit-license.org MIT
+ *
+ * @group geometry
+ *
+ * @internal
+ *
+ * @coversDefaultClass \LongitudeOne\Spatial\DBAL\Types\Geometry\PointType
+ */
+class Point3DTypeTest extends PersistOrmTestCase
+{
+    use PersistantPointHelperTrait;
+
+    /**
+     * Set up the test.
+     */
+    protected function setUp(): void
+    {
+        $this->usesEntity(self::POINT3D_ENTITY);
+        // Three-dimensional point doesn't exist on MySQL
+        // $this->supportsPlatform(MySQLPlatform::class);
+        $this->supportsPlatform(PostgreSQLPlatform::class);
+        parent::setUp();
+    }
+
+    /**
+     * Test the declaration type.
+     */
+    public function testDeclarationType(): void
+    {
+        $this->usesEntity(self::POINT3D_ENTITY);
+        $metadata = $this->getEntityManager()->getClassMetadata(Point3DEntity::class);
+
+        // Set the type
+        $type = null;
+        if (is_array($metadata->getFieldMapping('point'))) {
+            // doctrine/orm:2.9
+            $type = $metadata->getFieldMapping('point')['type'];
+        }
+        if (is_object($metadata->getFieldMapping('point'))) {
+            // doctrine/orm:3.1, doctrine/orm:4.0
+            $type = $metadata->getFieldMapping('point')->type;
+        }
+
+        // Check the type
+        static::assertNotNull($type, 'This test is not compatible with this version of doctrine/orm');
+        static::assertEquals('point3d', $type);
+    }
+
+    /**
+     * Test to store a point and find it by its geometric.
+     */
+    public function testFindByPoint(): void
+    {
+        $point = new Point3D(1, 2, 3);
+        $entity = new Point3DEntity();
+
+        $entity->setPoint($point);
+        $this->getEntityManager()->persist($entity);
+        $this->getEntityManager()->flush();
+
+        $result = $this->getEntityManager()->getRepository(self::POINT3D_ENTITY)->findByPoint($point);
+
+        static::assertEquals($entity, $result[0]);
+    }
+
+    /**
+     * Unit test the getName, getSQLType and getBindingType methods.
+     *
+     * @throws TypeNotRegistered It shall not happen
+     */
+    public function testName(): void
+    {
+        if (!Type::hasType('point3d')) {
+            Type::addType('point3d', Point3DType::class);
+        }
+
+        $spatialInstance = new Point3DType();
+        static::assertNotFalse($spatialInstance->getName());
+        static::assertSame('point3d', $spatialInstance->getName());
+        static::assertSame(ParameterType::STRING, $spatialInstance->getBindingType());
+        static::assertSame('POINTZ', $spatialInstance->getSQLType());
+    }
+
+    /**
+     * Test to store a null point and find it by its id.
+     */
+    public function testNullPoint(): void
+    {
+        $pointEntity = new Point3DEntity();
+        $this->getEntityManager()->persist($pointEntity);
+        $this->getEntityManager()->flush();
+        static::assertIsRetrievableById($this->getEntityManager(), $pointEntity);
+    }
+
+    /**
+     * Test to store a point and find it by its id.
+     */
+    public function testPoint(): void
+    {
+        $point = new Point3D(1, 2, 3);
+        $pointEntity = new Point3DEntity();
+        $pointEntity->setPoint($point);
+        $this->getEntityManager()->persist($pointEntity);
+        $this->getEntityManager()->flush();
+
+        static::assertIsRetrievableById($this->getEntityManager(), $pointEntity);
+    }
+}
--- tests/LongitudeOne/Spatial/Tests/OrmTestCase.php
+++ tests/LongitudeOne/Spatial/Tests/OrmTestCase.php
@@ -38,6 +38,7 @@ use Doctrine\ORM\Tools\ToolsException;
 use LongitudeOne\Spatial\DBAL\Types\AbstractSpatialType;
 use LongitudeOne\Spatial\DBAL\Types\DoctrineSpatialTypeInterface;
 use LongitudeOne\Spatial\DBAL\Types\Geography\LineStringType as GeographyLineStringType;
+use LongitudeOne\Spatial\DBAL\Types\Geography\Point3DType as GeographyPoint3DType;
 use LongitudeOne\Spatial\DBAL\Types\Geography\PointType as GeographyPointType;
 use LongitudeOne\Spatial\DBAL\Types\Geography\PolygonType as GeographyPolygonType;
 use LongitudeOne\Spatial\DBAL\Types\GeographyType;
@@ -45,6 +46,7 @@ use LongitudeOne\Spatial\DBAL\Types\Geometry\LineStringType;
 use LongitudeOne\Spatial\DBAL\Types\Geometry\MultiLineStringType;
 use LongitudeOne\Spatial\DBAL\Types\Geometry\MultiPointType;
 use LongitudeOne\Spatial\DBAL\Types\Geometry\MultiPolygonType;
+use LongitudeOne\Spatial\DBAL\Types\Geometry\Point3DType;
 use LongitudeOne\Spatial\DBAL\Types\Geometry\PointType;
 use LongitudeOne\Spatial\DBAL\Types\Geometry\PolygonType;
 use LongitudeOne\Spatial\DBAL\Types\GeometryType;
@@ -151,6 +153,7 @@ use LongitudeOne\Spatial\Tests\Doctrine\Logging\FileLogger;
 use LongitudeOne\Spatial\Tests\Fixtures\GeographyEntity;
 use LongitudeOne\Spatial\Tests\Fixtures\GeoLineStringEntity;
 use LongitudeOne\Spatial\Tests\Fixtures\GeometryEntity;
+use LongitudeOne\Spatial\Tests\Fixtures\GeoPoint3DEntity;
 use LongitudeOne\Spatial\Tests\Fixtures\GeoPointSridEntity;
 use LongitudeOne\Spatial\Tests\Fixtures\GeoPolygonEntity;
 use LongitudeOne\Spatial\Tests\Fixtures\LineStringEntity;
@@ -158,6 +161,7 @@ use LongitudeOne\Spatial\Tests\Fixtures\MultiLineStringEntity;
 use LongitudeOne\Spatial\Tests\Fixtures\MultiPointEntity;
 use LongitudeOne\Spatial\Tests\Fixtures\MultiPolygonEntity;
 use LongitudeOne\Spatial\Tests\Fixtures\NoHintGeometryEntity;
+use LongitudeOne\Spatial\Tests\Fixtures\Point3DEntity;
 use LongitudeOne\Spatial\Tests\Fixtures\PointEntity;
 use LongitudeOne\Spatial\Tests\Fixtures\PolygonEntity;
 
@@ -171,6 +175,7 @@ abstract class OrmTestCase extends SpatialTestCase
 {
     // Fixtures and entities
     public const GEO_LINESTRING_ENTITY = GeoLineStringEntity::class;
+    public const GEO_POINT_3D_ENTITY = GeoPoint3DEntity::class;
     public const GEO_POINT_SRID_ENTITY = GeoPointSridEntity::class;
     public const GEO_POLYGON_ENTITY = GeoPolygonEntity::class;
     public const GEOGRAPHY_ENTITY = GeographyEntity::class;
@@ -180,6 +185,7 @@ abstract class OrmTestCase extends SpatialTestCase
     public const MULTIPOINT_ENTITY = MultiPointEntity::class;
     public const MULTIPOLYGON_ENTITY = MultiPolygonEntity::class;
     public const NO_HINT_GEOMETRY_ENTITY = NoHintGeometryEntity::class;
+    public const POINT3D_ENTITY = Point3DEntity::class;
     public const POINT_ENTITY = PointEntity::class;
     public const POLYGON_ENTITY = PolygonEntity::class;
 
@@ -210,6 +216,10 @@ abstract class OrmTestCase extends SpatialTestCase
         PointEntity::class => [
             'types' => ['point'],
             'table' => 'PointEntity',
+        ],
+        Point3DEntity::class => [
+            'types' => ['point3d'],
+            'table' => 'Point3DEntity',
         ],
         LineStringEntity::class => [
             'types' => ['linestring'],
@@ -247,6 +257,10 @@ abstract class OrmTestCase extends SpatialTestCase
             'types' => ['geopolygon'],
             'table' => 'GeoPolygonEntity',
         ],
+        GeoPoint3DEntity::class => [
+            'types' => ['geopoint3d'],
+            'table' => 'GeoPoint3DEntity',
+        ],
     ];
 
     /**
@@ -255,6 +269,7 @@ abstract class OrmTestCase extends SpatialTestCase
     protected static array $types = [
         'geometry' => GeometryType::class,
         'point' => PointType::class,
+        'point3d' => Point3DType::class,
         'linestring' => LineStringType::class,
         'polygon' => PolygonType::class,
         'multipoint' => MultiPointType::class,
@@ -262,6 +277,7 @@ abstract class OrmTestCase extends SpatialTestCase
         'multipolygon' => MultiPolygonType::class,
         'geography' => GeographyType::class,
         'geopoint' => GeographyPointType::class,
+        'geopoint3d' => GeographyPoint3DType::class,
         'geolinestring' => GeographyLineStringType::class,
         'geopolygon' => GeographyPolygonType::class,
     ];
