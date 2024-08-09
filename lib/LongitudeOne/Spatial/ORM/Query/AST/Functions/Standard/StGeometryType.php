f1a632f9d7cd4fc12648ecf58a1acef4cf6953be
Adrien Crivelli <adrien.crivelli@gmail.com>
2024-08-09 15:59:41 +0200

    Standard function are not compatible if specialized functions exist

--- lib/LongitudeOne/Spatial/ORM/Query/AST/Functions/Standard/StGeometryType.php
+++ lib/LongitudeOne/Spatial/ORM/Query/AST/Functions/Standard/StGeometryType.php
@@ -19,7 +19,6 @@ declare(strict_types=1);
 namespace LongitudeOne\Spatial\ORM\Query\AST\Functions\Standard;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
-use Doctrine\DBAL\Platforms\MySQLPlatform;
 use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
 use LongitudeOne\Spatial\ORM\Query\AST\Functions\AbstractSpatialDQLFunction;
 
@@ -75,6 +74,6 @@ class StGeometryType extends AbstractSpatialDQLFunction
      */
     protected function getPlatforms(): array
     {
-        return [PostgreSQLPlatform::class, MySQLPlatform::class];
+        return [PostgreSQLPlatform::class];
     }
 }
