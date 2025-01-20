f875133af72a82b1d67dbacd107dc770169e1e9a
Alexandre Tranchant <github@lex.mozmail.com>
2025-01-20 17:50:19 +0100

    Copyright 2025 (#114)

--- tests/LongitudeOne/Spatial/Tests/ORM/Query/AST/Functions/MySql/SpMbrWithinTest.php
+++ tests/LongitudeOne/Spatial/Tests/ORM/Query/AST/Functions/MySql/SpMbrWithinTest.php
@@ -5,8 +5,8 @@
  * PHP 8.1 | 8.2 | 8.3
  * Doctrine ORM 2.19 | 3.1
  *
- * Copyright Alexandre Tranchant <alexandre.tranchant@gmail.com> 2017-2024
- * Copyright Longitude One 2020-2024
+ * Copyright Alexandre Tranchant <alexandre.tranchant@gmail.com> 2017-2025
+ * Copyright Longitude One 2020-2025
  * Copyright 2015 Derek J. Lambert
  *
  * For the full copyright and license information, please view the LICENSE
